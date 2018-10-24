f8554a5310333971ecd7528df7db2dd3cd33e420
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-10-23 20:45:55 -0700

    created initial version of how maze generation will be able to interact with individual nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+    public Node Up { get; set; }
+    public Node Down { get; set; }
+
+    private GameObject leftWall;
+    private GameObject rightWall;
+    private GameObject topWall;
+    private GameObject bottomWall;
+
+    void Start ()
+    {
+        leftWall = transform.Find("Left Wall").gameObject;
+        rightWall = transform.Find("Right Wall").gameObject;
+        topWall = transform.Find("Top Wall").gameObject;
+        bottomWall = transform.Find("Bottom Wall").gameObject;
+    }
+
+    public void DestroyLeft ()
+    {
+        Destroy(leftWall);
+    }
+
+    public void DestroyRight()
+    {
+        Destroy(rightWall);
+    }
+
+    public void DestroyTop()
+    {
+        Destroy(topWall);
+    }
+
+    public void DestroyBottom()
+    {
+        Destroy(bottomWall);
+    }
+}
