adbb9096f4629af11257a92808e70c7d0fff71c0
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-10-26 16:20:11 -0700

    created extremely basic version of enemy

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        controller.SimpleMove(direction * speed);
+	}
+}
