e04a2058b024eaf51c8666540034bc69590c0a36
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-10-28 15:07:38 -0700

    added big boy to walk about

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,21 +7,24 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public GameObject player;
     private NavMeshAgent agent;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = transform.Find("dude").GetComponent<Animator>();
+        agent.speed = transform.localScale.x;
+        anim.Play("Walk", -1, 0.0f);
         StartCoroutine(EnableAgent());
 	}
 
     IEnumerator EnableAgent ()
     {
-        yield return null;
-        agent.enabled = true;
         while (true)
         {
             agent.destination = player.transform.position;
+            anim.speed = agent.velocity.magnitude / agent.speed;
             yield return new WaitForSeconds(0.1f);
         }
     }
